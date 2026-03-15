92cb711b994e05ab8b29d3e2f3897e9c82ab66f2
Samandar Takhirov <samandar@udevs.io>
2026-03-15 22:16:18 +0500

    chore: Bump version to 1.0.0+15, update build number, and add new dependencies for telegram_web_app and expandable_page_view

--- lib/src/common/constant/pubspec.yaml.g.dart
+++ lib/src/common/constant/pubspec.yaml.g.dart
@@ -92,13 +92,13 @@ sealed class Pubspec {
   static const PubspecVersion version = (
     /// Non-canonical string representation of the version as provided
     /// in the pubspec.yaml file.
-    representation: r'1.0.0+14',
+    representation: r'1.0.0+15',
 
     /// Returns a 'canonicalized' representation
     /// of the application version.
     /// This represents the version string in accordance with
     /// Semantic Versioning (SemVer) standards.
-    canonical: r'1.0.0+14',
+    canonical: r'1.0.0+15',
 
     /// MAJOR version when you make incompatible API changes.
     /// The major version number: 1 in "1.2.3".
@@ -117,7 +117,7 @@ sealed class Pubspec {
     preRelease: <String>[],
 
     /// The build identifier: "foo" in "1.2.3+foo".
-    build: <String>[r'14'],
+    build: <String>[r'15'],
   );
 
   /// Build date and time (UTC)
@@ -125,11 +125,11 @@ sealed class Pubspec {
     2026,
     3,
     15,
-    12,
-    46,
-    1,
-    643,
-    772,
+    17,
+    13,
+    23,
+    507,
+    412,
   );
 
   /// Name
@@ -447,6 +447,8 @@ sealed class Pubspec {
     'flutter_local_notifications': r'^20.1.0',
     'cupertino_icons': r'^1.0.8',
     'octopus': r'^0.0.9',
+    'telegram_web_app': r'^0.3.3',
+    'expandable_page_view': r'^1.2.1',
   };
 
   /// Developer dependencies
