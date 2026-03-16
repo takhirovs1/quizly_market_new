f071b48f288b1f63d96ba2afb1fa5ad3267a5dfd
Abrorjon755 <Abrorjonbahodirov@gmail.com>
2026-03-16 13:15:51 +0500

    chore: Update Firebase hosting configuration and adjust package versions in pubspec.lock and pubspec.yaml.

--- lib/src/common/constant/pubspec.yaml.g.dart
+++ lib/src/common/constant/pubspec.yaml.g.dart
@@ -92,13 +92,13 @@ sealed class Pubspec {
   static const PubspecVersion version = (
     /// Non-canonical string representation of the version as provided
     /// in the pubspec.yaml file.
-    representation: r'1.0.0+6',
+    representation: r'1.0.0+7',
 
     /// Returns a 'canonicalized' representation
     /// of the application version.
     /// This represents the version string in accordance with
     /// Semantic Versioning (SemVer) standards.
-    canonical: r'1.0.0+6',
+    canonical: r'1.0.0+7',
 
     /// MAJOR version when you make incompatible API changes.
     /// The major version number: 1 in "1.2.3".
@@ -117,7 +117,7 @@ sealed class Pubspec {
     preRelease: <String>[],
 
     /// The build identifier: "foo" in "1.2.3+foo".
-    build: <String>[r'6'],
+    build: <String>[r'7'],
   );
 
   /// Build date and time (UTC)
@@ -126,10 +126,10 @@ sealed class Pubspec {
     3,
     13,
     7,
-    9,
-    34,
-    425,
-    220,
+    21,
+    24,
+    709,
+    656,
   );
 
   /// Name
