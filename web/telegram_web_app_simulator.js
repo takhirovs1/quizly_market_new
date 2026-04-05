90577534f03ef933d543f793823de856401c93b9
Samandar Takhirov <samandar@udevs.io>
2026-04-05 17:07:49 +0500

    fix: Update DEFAULT_TELEGRAM_INIT_DATA with new auth_date and signature for improved security and functionality.

--- web/telegram_web_app_simulator.js
+++ web/telegram_web_app_simulator.js
@@ -9,8 +9,7 @@
    */
 
   const DEFAULT_TELEGRAM_INIT_DATA =
-    "user=%7B%22id%22%3A1251798314%2C%22first_name%22%3A%22Takhirov%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22Takhirovs%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FPv8sJMzN7dZuMSud-i2sgBEsm1XHx-1alxOrHEO5BX8.svg%22%7D&chat_instance=995270295019096179&chat_type=sender&auth_date=1775315100&signature=FZ5zb4-mXM3vckCWS7KppcvFnRsJfMkBOos6Due45Hlp7j1wYlXTvroFUA_nOuJe8nr5MbsNsGtW2dSD-eQsCg&hash=846a31496ceef73c9177f36c840875454900319a5def44226aa4efe3e8404cce";
-
+"user=%7B%22id%22%3A1251798314%2C%22first_name%22%3A%22Takhirov%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22Takhirovs%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FPv8sJMzN7dZuMSud-i2sgBEsm1XHx-1alxOrHEO5BX8.svg%22%7D&chat_instance=995270295019096179&chat_type=sender&auth_date=1775390597&signature=X2RzbxtO4Wmc21jCmmmkclheQz4zeMkPggjkuiecUChBBp0zl5A2hV_ymlafIYu0cyvFQsyZ2d8ZsSJaWh51BQ&hash=6144c87a0feea7fe185449c68a7cbdecbaf0ef5a848f24de642b7f81993e5303";
 
   const parseDefaultInitData = (raw) => {
     const sp = new URLSearchParams(raw);
