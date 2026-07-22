cd0b4252a6a30323072bc3ac3ff50054aa33aa83
Samandar Takhilov <samandar@udevs.io>
2026-07-22 15:35:02 +0500

    chore: bump build number to 228 and update generated version metadata

--- lib/src/common/constant/pubspec.yaml.g.dart
+++ lib/src/common/constant/pubspec.yaml.g.dart
@@ -92,13 +92,13 @@ sealed class Pubspec {
   static const PubspecVersion version = (
     /// Non-canonical string representation of the version as provided
     /// in the pubspec.yaml file.
-    representation: r'1.0.1+227',
+    representation: r'1.0.1+228',
 
     /// Returns a 'canonicalized' representation
     /// of the application version.
     /// This represents the version string in accordance with
     /// Semantic Versioning (SemVer) standards.
-    canonical: r'1.0.1+227',
+    canonical: r'1.0.1+228',
 
     /// MAJOR version when you make incompatible API changes.
     /// The major version number: 1 in "1.2.3".
@@ -117,7 +117,7 @@ sealed class Pubspec {
     preRelease: <String>[],
 
     /// The build identifier: "foo" in "1.2.3+foo".
-    build: <String>[r'227'],
+    build: <String>[r'228'],
   );
 
   /// Build date and time (UTC)
@@ -125,11 +125,11 @@ sealed class Pubspec {
     2026,
     7,
     22,
-    6,
-    22,
-    6,
+    10,
     31,
-    194,
+    8,
+    254,
+    569,
   );
 
   /// Name
