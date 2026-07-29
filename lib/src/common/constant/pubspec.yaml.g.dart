a1b7c152686408c8b0e8b2a39528f9667683c6e5
Samandar Takhilov <samandar@udevs.io>
2026-07-29 09:38:46 +0500

    chore: bump application version to 1.0.1+230

--- lib/src/common/constant/pubspec.yaml.g.dart
+++ lib/src/common/constant/pubspec.yaml.g.dart
@@ -92,13 +92,13 @@ sealed class Pubspec {
   static const PubspecVersion version = (
     /// Non-canonical string representation of the version as provided
     /// in the pubspec.yaml file.
-    representation: r'1.0.1+228',
+    representation: r'1.0.1+230',
 
     /// Returns a 'canonicalized' representation
     /// of the application version.
     /// This represents the version string in accordance with
     /// Semantic Versioning (SemVer) standards.
-    canonical: r'1.0.1+228',
+    canonical: r'1.0.1+230',
 
     /// MAJOR version when you make incompatible API changes.
     /// The major version number: 1 in "1.2.3".
@@ -117,19 +117,19 @@ sealed class Pubspec {
     preRelease: <String>[],
 
     /// The build identifier: "foo" in "1.2.3+foo".
-    build: <String>[r'228'],
+    build: <String>[r'230'],
   );
 
   /// Build date and time (UTC)
   static final DateTime timestamp = DateTime.utc(
     2026,
     7,
-    22,
-    10,
-    31,
+    23,
     8,
-    254,
-    569,
+    47,
+    19,
+    43,
+    790,
   );
 
   /// Name
